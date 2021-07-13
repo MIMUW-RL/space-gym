a80cb3bc3934ea0c81328826baffddcbb58d5ed2
Kajetan Janiak <kajetan.janiak@gmail.com>
2021-07-13 16:32:37 +0200

    added time limit in envs registration

--- gym_space/__init__.py
+++ gym_space/__init__.py
@@ -5,11 +5,13 @@ from gym.envs.registration import register
 register(
     id="DoNotCrashDiscrete-v0",
     entry_point="gym_space.envs:DoNotCrashDiscreteEnv",
+    max_episode_steps=300
 )
 
 register(
     id="DoNotCrashContinuous-v0",
     entry_point="gym_space.envs:DoNotCrashContinuousEnv",
+    max_episode_steps=300
 )
 
 
@@ -18,9 +20,11 @@ register(
 register(
     id="GoalDiscrete-v0",
     entry_point="gym_space.envs:GoalDiscreteEnv",
+    max_episode_steps=300
 )
 
 register(
     id="GoalContinuous-v0",
     entry_point="gym_space.envs:GoalContinuousEnv",
+    max_episode_steps=300
 )
